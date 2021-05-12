d2986cc43131f218f04b5561f6ae777295e105c7
Omar Benhmuda <omar2kb@gmail.com>
2021-05-12 18:52:57 -0400

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -20,28 +20,9 @@ app.use((req, res, next) => {
   next();
 })
 
-
-
-
-
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config);
-  connection.connect(function (err) {
-    if (err) {
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000);
-    }
-  });
-  connection.on('error', function (err) {
-    console.log('db error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
+app.get(["/","/index.html.var"], (req, res) => {
+  res.sendFile(path.join(__dirname + '../dist/workstudy-site/index.html'));
+});
 
 
 app.get('/data/:graphName', async (req, res) => {
